efc181968d7457d00be6bc231b8e07b7bc1deca8
rezowan311410 <114556818+rezowan311410@users.noreply.github.com>
2023-02-07 17:38:23 +0600

    try

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const chalk = require('chalk');
-const app = express()
+const cors = require('cors');
+const app = express();
+const userRouter = require("./routes/userRoutes")
+//middle ware
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
 
 app.get('/', function (req, res) {
   res.json([
@@ -17,6 +24,8 @@ app.get('/test', function (req, res) {
     }
   ])
 })
+
+app.use("/",userRouter);
 
 app.listen(7000,()=>{
     console.log(chalk.red("server connected"));
