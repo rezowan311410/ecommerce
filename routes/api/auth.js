954c7fab9adfbbecef5e370548f5ca6609400a69
rezowan311410 <114556818+rezowan311410@users.noreply.github.com>
2023-02-20 10:48:52 +0600

    user data

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const _ = express.Router();
-
+const user = require("../../models/user.js");
 _.post('/registration',(req,res)=>{
-    let {email,password,fullname}= req.body;
+    let {email,password,phone,firstname,lastname}= req.body;
 
-    if(!email){
-        res.json("you must enter an email")
-    }
-    if(!password){
-        res.json("you must enter an password")
-    }
-    if(!fullname){
-        res.json("you must enter an fullname")
-    }
+    // if(!email){
+    //     res.json("you must enter an email")
+    // }
+    // if(!password){
+    //     res.json("you must enter an password")
+    // }
+    // if(!phone){
+    //     res.json("you must enter an fullname")
+    // }
 
-    res.json({email,password,fullname});
+    const data = new user ({
+                email,
+                password,
+                phone,
+                firstname,
+                lastname
+                })
+
+    res.json(data);
 
 })
 
